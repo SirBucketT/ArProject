d9389da9a61da919625d99245b7d24f21e5d13d8
Theodor Kvarnström <tkpriva@icloud.com>
2025-11-06 18:17:33 +0100

    last lil update for today

--- Assets/Scripts/Questions/QuestionManager.cs
+++ Assets/Scripts/Questions/QuestionManager.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class QuestionManager : MonoBehaviour
 {
-    [Header("Question Data")]
+    [Header("Question")]
     [SerializeField] string question;
+    
+    [Header("Question Answers")]
     
     [SerializeField] string answerA;
     [SerializeField] string answerB;
     [SerializeField] string answerC;
     [SerializeField] string answerD;
     
+    [Header("TextMesh components")]
+    
     [SerializeField] TMP_Text answerTextA;
     [SerializeField] TMP_Text answerTextB;
     [SerializeField] TMP_Text answerTextC;
     [SerializeField] TMP_Text answerTextD;
+    [SerializeField] TMP_Text Question;
 
     void Start()
     {
+        Question.text = question;
         answerTextA.text = answerA;
         answerTextB.text = answerB;
         answerTextC.text = answerC;
