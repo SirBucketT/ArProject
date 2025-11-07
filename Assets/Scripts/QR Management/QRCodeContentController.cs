ea8fb76b3aaf0a5caeb1bb073ac8ae2e9ccc0fa5
Theodor Kvarnström <tkpriva@icloud.com>
2025-11-07 10:13:50 +0100

    update

---
+++ Assets/Scripts/QR Management/QRCodeContentController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+
+public class QRCodeContentController : MonoBehaviour
+{
+    [SerializeField]
+    private ARTrackedImageManager m_TrackedImageManager;
+    
+    [Header("AR Content Mapping")]
+    public ARContentMapping[] contentMappings;
+    
+    private Dictionary<string, GameObject> m_InstantiatedContent = new Dictionary<string, GameObject>();
+    
+    [System.Serializable]
+    public struct ARContentMapping
+    {
+        public string qrCodeName; 
+        public GameObject contentPrefab; 
+    }
+
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackablesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackablesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            string imageName = trackedImage.referenceImage.name;
+            
+            GameObject prefabToSpawn = GetPrefabForName(imageName);
+            
+            if (prefabToSpawn != null && !m_InstantiatedContent.ContainsKey(imageName))
+            {
+                GameObject newARContent = Instantiate(prefabToSpawn, trackedImage.transform);
+                m_InstantiatedContent.Add(imageName, newARContent);
+            }
+        }
+        
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            if (m_InstantiatedContent.TryGetValue(trackedImage.referenceImage.name, out GameObject content))
+            {
+                content.SetActive(trackedImage.trackingState == TrackingState.Tracking);
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            string imageName = trackedImage.Value.name;
+            if (m_InstantiatedContent.TryGetValue(imageName, out GameObject contentToRemove))
+            {
+                Destroy(contentToRemove);
+                m_InstantiatedContent.Remove(imageName);
+            }
+        }
+    }
+
+    private GameObject GetPrefabForName(string name)
+    {
+        foreach (var mapping in contentMappings)
+        {
+            if (mapping.qrCodeName == name)
+            {
+                return mapping.contentPrefab;
+            }
+        }
+        return null;
+    }
+}
