1371864f428ed9919630770d4efb8182816b6907
Theodor Kvarnström <tkpriva@icloud.com>
2025-11-11 18:39:46 +0100

    update

---
+++ Assets/Scripts/Messages/QuestionCheatSheetMessage.cs
@@ -0,0 +1,6 @@
+public class QuestionCheatSheetMessage : IMessage
+{
+    public bool hasClickedQuestion1 { get; set; }
+    public bool hasClickedQuestion2 { get; set; }
+    public bool hasClickedQuestion3 { get; set; }    
+}
--- Assets/Scripts/UI/SideBarUi.cs
+++ Assets/Scripts/UI/SideBarUi.cs
@@ -25,4 +25,20 @@ public class SideBarUi : MonoBehaviour
     {
         sidebarAnimator.SetBool(IsHiddenHash, true);
     }
+    
+    public void Question1() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true);
+        new QuestionCheatSheetMessage(){hasClickedQuestion1 = true}.InvokeExtension();
+    }
+    public void Question2() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true);
+        new QuestionCheatSheetMessage(){hasClickedQuestion2 = true}.InvokeExtension();
+    }
+    public void Question3() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion3 = true}.InvokeExtension();
+    }
 }
