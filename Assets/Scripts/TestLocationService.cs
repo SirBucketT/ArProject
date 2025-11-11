0d290856233e3bb684ca2ae86a4d8f10a86813b4
Theodor Kvarnström <tkpriva@icloud.com>
2025-11-11 13:44:38 +0100

    update

--- Assets/Scripts/TestLocationService.cs
+++ Assets/Scripts/TestLocationService.cs
@@ -7,56 +7,64 @@ public class TestLocationService : MonoBehaviour
     public static TestLocationService instance;
     
     public string location;
+    
     [SerializeField] TMP_Text locationText;
     
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     IEnumerator Start()
     {
-        // Check if the user has location service enabled.
         if (!Input.location.isEnabledByUser)
             Debug.Log("Location not enabled on device or app does not have permission to access location");
-
-        // Starts the location service.
         
         float desiredAccuracyInMeters = 10f;
         float updateDistanceInMeters = 10f;
-
+        
         Input.location.Start(desiredAccuracyInMeters, updateDistanceInMeters);
-
-        // Waits until the location service initializes
+        
         int maxWait = 20;
         while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
         {
             yield return new WaitForSeconds(1);
             maxWait--;
         }
-
-        // If the service didn't initialize in 20 seconds this cancels location service use.
+        
         if (maxWait < 1)
         {
             Debug.Log("Timed out");
+            location = "Initialization timed out.";
+            locationText.text = location;
             yield break;
         }
-
-        // If the connection failed this cancels location service use.
+        
         if (Input.location.status == LocationServiceStatus.Failed)
         {
             Debug.LogError("Unable to determine device location");
-
             location = "Enable location service!";
+            locationText.text = location;
             yield break;
         }
         else
         {
-            // If the connection succeeded, this retrieves the device's current location and displays it in the Console window.
-            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-
-            location = $"{Input.location.lastData.latitude:F6}, {Input.location.lastData.longitude:F6}";
-
-
+            LocationInfo lastData = Input.location.lastData;
+            
+            Debug.Log($"Location: {lastData.latitude} {lastData.longitude} {lastData.altitude}");
+            
+            location = $"{lastData.latitude:F6}, {lastData.longitude:F6}";
+            
             locationText.text = location;
         }
-
-        // Stops the location service if there is no need to query location updates continuously.
+        
         Input.location.Stop();
     }
 }
