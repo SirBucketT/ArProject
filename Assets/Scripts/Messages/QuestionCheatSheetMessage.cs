7ec1c1267d0a5926caa81baf8163d58d497b5400
Theodor Kvarnström <tkpriva@icloud.com>
2025-11-12 12:39:35 +0100

    update

--- Assets/Scripts/Messages/QuestionCheatSheetMessage.cs
+++ Assets/Scripts/Messages/QuestionCheatSheetMessage.cs
@@ -3,4 +3,10 @@ public class QuestionCheatSheetMessage : IMessage
     public bool hasClickedQuestion1 { get; set; }
     public bool hasClickedQuestion2 { get; set; }
     public bool hasClickedQuestion3 { get; set; }    
+    public bool hasClickedQuestion4 { get; set; }   
+    public bool hasClickedQuestion5 { get; set; }
+    public bool hasClickedQuestion6 { get; set; }
+    public bool hasClickedQuestion7 { get; set; }
+    public bool hasClickedQuestion8 { get; set; }
+    public bool hasClickedQuestion9 { get; set; }
 }
--- Assets/Scripts/UI/SideBarUi.cs
+++ Assets/Scripts/UI/SideBarUi.cs
@@ -41,4 +41,34 @@ public class SideBarUi : MonoBehaviour
         sidebarAnimator.SetBool(IsHiddenHash, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion3 = true}.InvokeExtension();
     }
+    public void Question4() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion4 = true}.InvokeExtension();
+    }
+    public void Question5() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion5 = true}.InvokeExtension();
+    }
+    public void Question6() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion6 = true}.InvokeExtension();
+    }
+    public void Question7() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion7 = true}.InvokeExtension();
+    }
+    public void Question8() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion8 = true}.InvokeExtension();
+    }
+    public void Question9() 
+    { 
+        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        new QuestionCheatSheetMessage(){hasClickedQuestion9 = true}.InvokeExtension();
+    }
 }
