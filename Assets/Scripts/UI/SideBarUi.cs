03708d3bc453cfe705a0f68f3b3370582e7b569c
Theodor Kvarnström <tkpriva@icloud.com>
2025-11-12 13:26:13 +0100

    update

--- Assets/Scripts/UI/SideBarUi.cs
+++ Assets/Scripts/UI/SideBarUi.cs
@@ -6,69 +6,69 @@ public class SideBarUi : MonoBehaviour
     
     Animator sidebarAnimator;
     
-    private readonly int IsHiddenHash = Animator.StringToHash("IsHidden");
+    private readonly int IsHiddenAnim = Animator.StringToHash("IsHidden");
 
     private void Awake()
     {
         sidebarAnimator = sideBar.GetComponent<Animator>();
-        sidebarAnimator.SetBool(IsHiddenHash, true);
+        sidebarAnimator.SetBool(IsHiddenAnim, true);
         sideBar.SetActive(false);
     }
     
     public void ShowSideBar()
     {
         sideBar.SetActive(true);
-        sidebarAnimator.SetBool(IsHiddenHash, false);
+        sidebarAnimator.SetBool(IsHiddenAnim, false);
     }
     
     public void HideSideBar()
     {
-        sidebarAnimator.SetBool(IsHiddenHash, true);
+        sidebarAnimator.SetBool(IsHiddenAnim, true);
     }
     
     public void Question1() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true);
+        sidebarAnimator.SetBool(IsHiddenAnim, true);
         new QuestionCheatSheetMessage(){hasClickedQuestion1 = true}.InvokeExtension();
     }
     public void Question2() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true);
+        sidebarAnimator.SetBool(IsHiddenAnim, true);
         new QuestionCheatSheetMessage(){hasClickedQuestion2 = true}.InvokeExtension();
     }
     public void Question3() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion3 = true}.InvokeExtension();
     }
     public void Question4() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion4 = true}.InvokeExtension();
     }
     public void Question5() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion5 = true}.InvokeExtension();
     }
     public void Question6() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion6 = true}.InvokeExtension();
     }
     public void Question7() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion7 = true}.InvokeExtension();
     }
     public void Question8() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion8 = true}.InvokeExtension();
     }
     public void Question9() 
     { 
-        sidebarAnimator.SetBool(IsHiddenHash, true); 
+        sidebarAnimator.SetBool(IsHiddenAnim, true); 
         new QuestionCheatSheetMessage(){hasClickedQuestion9 = true}.InvokeExtension();
     }
 }
